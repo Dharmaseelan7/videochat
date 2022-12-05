a94135fa62321d04cb6b03a8a57fa9f62f2d3e84
Dharmaseelan7 <87483092+Dharmaseelan7@users.noreply.github.com>
2022-12-05 19:11:43 +0530

    c

--- videochat/settings.py
+++ videochat/settings.py
@@ -120,7 +120,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
